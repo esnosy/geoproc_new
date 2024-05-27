8efc7d1f2f622a9ad93263d74748e31991e39a6f
Eyad Ahmed <iyadahmed430@gmail.com>
2024-05-27 20:46:11 +0300

    refactor: Improve Fixed_Point_64 class with static factory methods and compound assignment operators

--- fixed_point.hpp
+++ fixed_point.hpp
@@ -18,12 +18,20 @@ private:
   explicit Fixed_Point_64(uint64_t value) : value(value) {}
 
 public:
-  explicit Fixed_Point_64(float f) : value(std::round(f * inv_scale)) {}
-  explicit Fixed_Point_64(double f) : value(std::round(f * inv_scale)) {}
+  static Fixed_Point_64 from_float(float f) { return Fixed_Point_64(std::round(f * inv_scale)); }
+  static Fixed_Point_64 from_double(double f) { return Fixed_Point_64(std::round(f * inv_scale)); }
+  static Fixed_Point_64 from_parts(uint64_t integer, uint64_t fraction) {
+    return Fixed_Point_64(integer * inv_scale + fraction);
+  }
   Fixed_Point_64 operator+(const Fixed_Point_64 &other) const { return Fixed_Point_64(value + other.value); }
   Fixed_Point_64 operator-(const Fixed_Point_64 &other) const { return Fixed_Point_64(value - other.value); }
   Fixed_Point_64 operator*(const Fixed_Point_64 &other) const { return Fixed_Point_64((value * other.value) / inv_scale); }
   Fixed_Point_64 operator/(const Fixed_Point_64 &other) const { return Fixed_Point_64((value * inv_scale) / other.value); }
+  Fixed_Point_64 &operator+=(const Fixed_Point_64 &other) { return *this = *this + other; }
+  Fixed_Point_64 &operator-=(const Fixed_Point_64 &other) { return *this = *this - other; }
+  Fixed_Point_64 &operator*=(const Fixed_Point_64 &other) { return *this = *this * other; }
+  Fixed_Point_64 &operator/=(const Fixed_Point_64 &other) { return *this = *this / other; }
+  bool operator==(const Fixed_Point_64 &other) const { return value == other.value; }
   friend std::ostream &operator<<(std::ostream &os, const Fixed_Point_64 &number) {
     // https://stackoverflow.com/a/1714538/8094047
     return os << number.value / inv_scale << "." << std::setfill('0') << std::setw(num_fract_digits)
