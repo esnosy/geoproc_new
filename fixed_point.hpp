037775dd85f97f3a0d44633ae62262485db16382
Eyad Ahmed <iyadahmed430@gmail.com>
2024-05-27 15:44:23 +0300

    Fix missing leading zeros when printing fractional part of fixed point number

--- fixed_point.hpp
+++ fixed_point.hpp
@@ -2,13 +2,19 @@
 
 #include <cmath>
 #include <cstdint>
+#include <iomanip>
 #include <iostream>
 #include <ostream>
 
 class Fixed_Point_64 {
 private:
   uint64_t value = 0;
-  constexpr static uint64_t inv_scale = 1000;
+  constexpr static uint64_t num_fract_digits = 3;
+  constexpr static uint64_t inv_scale = [] {
+    uint64_t scale = 1;
+    for (uint64_t i = 0; i < num_fract_digits; i++) scale *= 10;
+    return scale;
+  }();
   explicit Fixed_Point_64(uint64_t value) : value(value) {}
 
 public:
@@ -19,6 +25,8 @@ public:
   Fixed_Point_64 operator*(const Fixed_Point_64 &other) const { return Fixed_Point_64((value * other.value) / inv_scale); }
   Fixed_Point_64 operator/(const Fixed_Point_64 &other) const { return Fixed_Point_64((value * inv_scale) / other.value); }
   friend std::ostream &operator<<(std::ostream &os, const Fixed_Point_64 &number) {
-    return os << number.value / inv_scale << "." << number.value % inv_scale;
+    // https://stackoverflow.com/a/1714538/8094047
+    return os << number.value / inv_scale << "." << std::setfill('0') << std::setw(num_fract_digits)
+              << number.value % inv_scale;
   }
 };
