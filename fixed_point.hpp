9a0fbebbad218f07b27f15346fce48fe25ced855
Eyad Ahmed <iyadahmed430@gmail.com>
2024-05-28 21:09:33 +0300

    Support negative fixed point values and refactor construction from integer value

--- fixed_point.hpp
+++ fixed_point.hpp
@@ -2,27 +2,26 @@
 
 #include <cmath>
 #include <cstdint>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <ostream>
 
 class Fixed_Point_64 {
 private:
-  uint64_t value = 0;
-  constexpr static uint64_t num_fract_digits = 3;
-  constexpr static uint64_t inv_scale = [] {
-    uint64_t result = 1;
-    for (uint64_t i = 0; i < num_fract_digits; i++) result *= 10;
+  int64_t value = 0;
+  constexpr static int64_t num_fract_digits = 3;
+  constexpr static int64_t inv_scale = [] {
+    int64_t result = 1;
+    for (int64_t i = 0; i < num_fract_digits; i++) result *= 10;
     return result;
   }();
-  explicit Fixed_Point_64(uint64_t value) : value(value) {}
+  explicit Fixed_Point_64(int64_t value) : value(value) {}
 
 public:
   static Fixed_Point_64 from_float(float f) { return Fixed_Point_64(std::round(f * inv_scale)); }
   static Fixed_Point_64 from_double(double f) { return Fixed_Point_64(std::round(f * inv_scale)); }
-  static Fixed_Point_64 from_parts(uint64_t integer, uint64_t fraction) {
-    return Fixed_Point_64(integer * inv_scale + fraction);
-  }
+  static Fixed_Point_64 from_unscaled(int64_t value) { return Fixed_Point_64(value); }
   Fixed_Point_64 operator+(const Fixed_Point_64 &other) const { return Fixed_Point_64(value + other.value); }
   Fixed_Point_64 operator-(const Fixed_Point_64 &other) const { return Fixed_Point_64(value - other.value); }
   Fixed_Point_64 operator*(const Fixed_Point_64 &other) const { return Fixed_Point_64((value * other.value) / inv_scale); }
@@ -33,8 +32,10 @@ public:
   Fixed_Point_64 &operator/=(const Fixed_Point_64 &other) { return *this = *this / other; }
   bool operator==(const Fixed_Point_64 &other) const { return value == other.value; }
   friend std::ostream &operator<<(std::ostream &os, const Fixed_Point_64 &number) {
+    if (number.value < 0) os << "-";
+    auto div = std::div(std::abs(number.value), inv_scale);
     // https://stackoverflow.com/a/1714538/8094047
-    return os << number.value / inv_scale << "." << std::setfill('0') << std::setw(num_fract_digits)
-              << number.value % inv_scale;
+    os << div.quot << "." << std::setfill('0') << std::setw(num_fract_digits) << div.rem;
+    return os;
   }
 };
--- fixed_point_demo.cpp
+++ fixed_point_demo.cpp
@@ -4,9 +4,11 @@
 #include "fixed_point.hpp"
 
 int main() {
-  auto a = Fixed_Point_64::from_parts(0, 50);
-  a /= Fixed_Point_64::from_parts(2, 0);
-  a *= Fixed_Point_64::from_parts(2, 0);
+  auto a = Fixed_Point_64::from_unscaled(50);
+  a /= Fixed_Point_64::from_unscaled(2000);
+  a *= Fixed_Point_64::from_unscaled(2000);
   std::cout << a << std::endl;
+
+  std::cout << Fixed_Point_64::from_unscaled(-100900) * Fixed_Point_64::from_unscaled(1000) << std::endl;
   return 0;
 }
